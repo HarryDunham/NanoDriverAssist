17af6d4b3bcbceeb8022869f237f5a3414d3c24a
harrydunham <harry.dunham@atomrain.com>
2022-01-31 21:02:16 -0500

    add cv2

--- entrypoint.py
+++ entrypoint.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
-import time
-import datetime
 import logging
-import sys
-# from systemd.journal import JournaldLogHandler
 from systemd.journal import JournalHandler
+import cv2
 
 logger = logging.getLogger(__name__)
-
-# journald_handler = JournaldLogHandler()
-
-# journald_handler.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
-
-# logger.addHandler(journald_handler)
-# handler = logging.StreamHandler(stream=sys.stdout)
 logger.addHandler(JournalHandler())
 logger.setLevel(logging.INFO)
-# formatter = logging.Formatter(fmt="%(asctime)s %(name)s.%(levelname)s: %(message)s")
-# handler.setFormatter(formatter)
-# logger.addHandler(handler)
 
-while True:
-    logger.info(datetime.datetime.now())
-    time.sleep(5)
 
+def show_webcam(mirror=False):
+    cam = cv2.VideoCapture(0)
+    while True:
+        ret_val, img = cam.read()
+        if mirror:
+            img = cv2.flip(img, 1)
+        cv2.imshow('my webcam', img)
+        if cv2.waitKey(1) == 27:
+            break  # esc to quit
+    cv2.destroyAllWindows()
+
+
+def main():
+    logger.info("Starting the webcam stream")
+    show_webcam()
+
+
+if __name__ == '__main__':
+    main()
